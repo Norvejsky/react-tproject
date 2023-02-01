9c9b25da72f050867e753ac3d045c25e55444d5f
Norvejsky <bezcor389@gmail.com>
2023-02-01 22:07:00 +0300

    added new commit

---
+++ src/components/Header.jsx
@@ -0,0 +1,11 @@
+import React from "react";
+
+const Header = () => {
+    return (
+        <header className='header'>
+            <img src='https://cdn.cnetcontent.com/93/b0/93b024ad-d3dc-4c0b-87f0-3898e450adb4.jpg' />
+        </header>
+    );
+}
+
+export default Header;
---
+++ src/components/Profile.jsx
@@ -0,0 +1,30 @@
+import React from "react";
+
+const Profile = () => {
+    return (
+        <div className='content'>
+          <div>
+            <img src='https://img3.goodfon.ru/original/800x480/3/5f/bereg-kamni-peyzazh.jpg'/>
+          </div>
+          <div>
+            ava
+          </div>
+          <div>
+            my posts
+            <div>
+              new post
+            </div>
+            <div>
+              <div>
+                post 1
+              </div>
+              <div>
+                post 2
+              </div>
+            </div>
+          </div>
+        </div>
+    );
+}
+
+export default Profile;
